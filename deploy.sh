34bc8bc4717525b115dbd743d5df5d9b8833c736
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-04 13:47:16 +0000

    fixed typo

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
 
   rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
 
-  ssh travis@142.93.227.119: "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
+  ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
 else
   echo "Not deploying, since the branch isn't master."
 fi
