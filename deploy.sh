68569f1700076d33be3b6428b0f636bde1070ca2
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-04 10:01:55 +0000

    trying to figure this out

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@142.93.227.119:/home/halldor/blog/client
-  rsync -a server/ travis@142.93.227.119:/home/halldor/blog/server
+  #rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@142.93.227.119:/home/halldor/blog/client
+  #rsync -a server/ travis@142.93.227.119:/home/halldor/blog/server
+  scp 'docker-compose.production.yml' travis@142.93.227.119:/home/halldor/blog
 else
   echo "Not deploying, since the branch isn't master."
 fi
