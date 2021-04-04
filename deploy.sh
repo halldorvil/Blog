199ec22ea8d24a5c6b3af2fe8522cc5ac0f75edd
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-04 16:01:10 +0000

    docker image version handling

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
+  sed -i "s/TAG/$SHA/g" docker-compose.production.yml
+
   rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
 
   ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
