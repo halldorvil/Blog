70e32289e2cbe85c33019c5dd3779ab828550ac2
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-04 10:15:38 +0000

    trying rsync

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
 
   #rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@142.93.227.119:/home/halldor/blog/client
   #rsync -a server/ travis@142.93.227.119:/home/halldor/blog/server
-  scp 'docker-compose.production.yml' travis@142.93.227.119:/home/halldor/blog
+  #scp 'docker-compose.production.yml' travis@142.93.227.119:/home/halldor/blog
+
+  rsync -a ~/docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
 else
   echo "Not deploying, since the branch isn't master."
 fi
