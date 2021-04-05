e36c7524585a36d56a873752ef7c1bcdce81dce1
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-05 18:16:04 +0000

    trying different things

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   sed -i "s/{TAG}/$SHA/g" docker-compose.production.yml
 
   rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
+  rsync -a init-letsencrypt.sh travis@142.93.227.119:/home/halldor/blog
 
+  ssh travis@142.93.227.119 "cd /home/halldor/blog ; rm docker-compose.yml ; mv docker-compose.production.yml docker-compose.yml"
   #ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
 else
   echo "Not deploying, since the branch isn't master."
