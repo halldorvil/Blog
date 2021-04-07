34959b6081c1bff5e91c5f93bec5f4539cf2012c
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-07 20:39:13 +0000

    modification to deployment script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,10 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
 
   sed -i "s/{TAG}/$SHA/g" docker-compose.production.yml
 
-  rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
-  rsync -a init-letsencrypt.sh travis@142.93.227.119:/home/halldor/blog
-  rsync -a ./data/nginx/prod/app.production.conf travis@142.93.227.119:/home/halldor/blog/data/nginx/prod
+  rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog
 
   ssh travis@142.93.227.119 "cd /home/halldor/blog ; rm docker-compose.yml ; mv docker-compose.production.yml docker-compose.yml"
-  #ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
+  ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
 else
   echo "Not deploying, since the branch isn't master."
 fi
