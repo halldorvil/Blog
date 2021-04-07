ebc64e15afa5615542199156da0c25fefc7d0e71
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-07 18:00:30 +0000

    some stuff

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  sed -i "s/{TAG}/$SHA/g" docker-compose.production.yml
+  #sed -i "s/{TAG}/$SHA/g" docker-compose.production.yml
 
-  rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
-  rsync -a init-letsencrypt.sh travis@142.93.227.119:/home/halldor/blog
+  #rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog 
+  #rsync -a init-letsencrypt.sh travis@142.93.227.119:/home/halldor/blog
 
-  ssh travis@142.93.227.119 "cd /home/halldor/blog ; rm docker-compose.yml ; mv docker-compose.production.yml docker-compose.yml"
+  #ssh travis@142.93.227.119 "cd /home/halldor/blog ; rm docker-compose.yml ; mv docker-compose.production.yml docker-compose.yml"
   #ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
 else
   echo "Not deploying, since the branch isn't master."
