34f3979181d4debb644d29e7c65d33f912fb9db8
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-07 20:44:36 +0000

    fixed an error in deployment script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   rsync -a docker-compose.production.yml travis@142.93.227.119:/home/halldor/blog
 
   ssh travis@142.93.227.119 "cd /home/halldor/blog ; rm docker-compose.yml ; mv docker-compose.production.yml docker-compose.yml"
-  ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose -f docker-compose.production.yml up -d"
+  ssh travis@142.93.227.119 "cd /home/halldor/blog ; docker-compose up -d"
 else
   echo "Not deploying, since the branch isn't master."
 fi
