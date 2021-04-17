8e174fca1f43e5af598383213cfe3500acd7dbe2
Halldor Vilhjalmsson <halldorvil@gmail.com>
2021-04-17 13:38:39 +0000

    using html for blog posts

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -4,15 +4,17 @@ import "../styles/BlogPost.css";
 
 const BlogPost = ({ post: { content, created_on, title } }) => {
   moment.locale("en");
-  
+
   return (
     <div className="blog-post">
       <div className="blog-post-content">
         <div className="blog-post-heading">
           <h1 className="blog-post-title">{title}</h1>
-          <p className="blog-post-date">{moment(created_on).format("MMM Do YYYY")}</p>
+          <p className="blog-post-date">
+            {moment(created_on).format("MMM Do YYYY")}
+          </p>
         </div>
-        <p className="blog-post-text">{content}</p>
+        <div className="blog-post-text" dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </div>
   );
